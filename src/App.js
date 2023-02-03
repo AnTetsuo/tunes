a7631cb733e6644f633a049e2ab8de200cdcba26
Andrey Tetsuo <tetsuo_miyasato@hotmail.com>
2023-02-03 15:03:00 -0300

    Requisito 1: estabelece rotas para cada componente

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div
+        className="App"
+      >
+        <Switch>
+          <Route path="/" component={ Login } exact />
+          <Route path="/search" component={ Search } exact />
+          <Route path="/album/:id" component={ Album } exact />
+          <Route path="/favorites" component={ Favorites } exact />
+          <Route path="/profile" component={ Profile } exact />
+          <Route path="/profile/edit" component={ ProfileEdit } exact />
+          <Route path="*" component={ NotFound } exact />
+        </Switch>
+      </div>
+    );
   }
 }
 
