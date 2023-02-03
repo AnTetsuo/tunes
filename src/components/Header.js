c8893aaf4feac76cfbe2b76d249ae66312b81ec8
Andrey Tetsuo <tetsuo_miyasato@hotmail.com>
2023-02-03 17:42:44 -0300

    Requisito 4: Links do header estabelecidos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
-import Loading from './Loading';
 
 class Header extends Component {
   constructor() {
@@ -42,6 +42,26 @@ class Header extends Component {
         >
           { loading ? 'Carregando...' : userGet.name }
         </p>
+        <div className="nav-links">
+          <Link
+            to="/search"
+            data-testid="link-to-search"
+          >
+            Search
+          </Link>
+          <Link
+            to="/favorites"
+            data-testid="link-to-favorites"
+          >
+            Favoriotes
+          </Link>
+          <Link
+            to="/profile"
+            data-testid="link-to-profile"
+          >
+            Profile
+          </Link>
+        </div>
       </header>
     );
   }
